8516365f008aee2d08f5cd4d17d237f8c1e382b8
Q4T <ukgarethevans@gmail.com>
2018-03-18 14:14:02 +0000

    adding dress class

---
+++ src/test/java/com/automationpractice/WEB_Tests/pages/DressSize.java
@@ -0,0 +1,25 @@
+package com.automationpractice.WEB_Tests.pages;
+
+public class DressSize {
+
+	public String small;
+	public String medium;
+	public String large;
+	
+	/* not sure how to proceed jet. 
+	 * I alos want classes for each of the 2 products to make selecting fields like size from them super easy in the step defs
+	 * this class will be classed from those product classes to..or will it, not sure yet this could all be contained in the product class.
+	 * 
+	 * 
+	 */
+	
+	
+		public void  woof() {
+		
+			 final String ONE = "1";
+			 final String TWO = "2";
+			 final String THREE = "3";
+		}
+	
+	
+}
